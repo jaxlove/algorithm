bedb24062d7dd83ebd3b49b5a638e576151d8401
wangdejun@tydic.com <dicoss@2019>
2020-08-25 22:56:34 +0800

    汉诺塔开发

--- src/main/java/practice/stackqueue/hanNuoTa/WdjTest.java
+++ src/main/java/practice/stackqueue/hanNuoTa/WdjTest.java
@@ -10,25 +10,8 @@ import java.util.Stack;
 public class WdjTest {
 
     public static void main(String[] args) {
-        Stack<Integer> data = new Stack<>();
-        data.push(14);
-        data.push(13);
-        data.push(12);
-        data.push(11);
-        data.push(10);
-        data.push(9);
-        data.push(8);
-        data.push(7);
-        data.push(6);
-        data.push(5);
-        data.push(4);
-        data.push(3);
-        data.push(2);
-        data.push(1);
-        new HanNuoTa(data);
-        System.out.println(HanNuoTa.left);
-        System.out.println(HanNuoTa.middle);
-        System.out.println(HanNuoTa.right);
+        int count = 3;
+        new HanNuoTa().handleData(count,'A','C','B');
     }
 
     static class HanNuoTa {
@@ -37,34 +20,16 @@ public class WdjTest {
         static Stack<Integer> middle = new Stack();
         static Stack<Integer> right = new Stack();
 
-        static int size = 0;
 
-        static int handlerCount = 0;
-
-        private static Target lastTarget = Target.NO;
-
-        public HanNuoTa(Stack<Integer> stack) {
-            left = stack;
-            size = stack.size();
+        void handleData(Integer num,char from ,char to,char free) {
+            if(num == 1){
+                System.out.println(num + " " + from + "-> "+ to);
+            }else {
+                handleData(num -1,from,free,to);
+                System.out.println(num + " " + from + "-> "+ to);
+                handleData(num-1,free,to,from);
+            }
         }
-
-        void handleData() {
-            // if 向将left 倒数x的数据移动到 空的C
-            //target.push(A.pop())
-            //然后 为了 将 B1移到C
-
-            //需要把 倒数B2的数据，移动到A上
-            //为了获取B2，需要将B3移动到C上
-            //为了B3放到C上，需要把B4移到A
-
-        }
-
-
     }
-
-    static enum Target {
-        NO, L, M, R;
-    }
-
 
 }
